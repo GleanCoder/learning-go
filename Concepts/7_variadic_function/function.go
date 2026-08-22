8e07bc96784faedb58db2eba8b83909ed38c0ad7
GleanCoder <adityakiranacharya0@gmail.com>
2026-08-23 02:47:06 +0530

    learn variadic function in golang

---
+++ Concepts/7_variadic_function/function.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(summation(3, 4, 5, 6))
+}
+
+/*
+- variadic function is the function which can take any number of arguments.
+
+How it works internally: inside the function, it is just a regular []variable_type slice. Go collects whatever arguments you pass into that slice automatically.
+
+Rules:
+
+Only the last parameter in a function can be variadic — you can't have two variadic params, and nothing can come after it
+You can mix regular params with one variadic one at the end
+
+*/
+
+func summation(num ...int) int {
+	sum := 0
+	for _, value := range num {
+		sum = sum + value
+	}
+
+	return sum
+}

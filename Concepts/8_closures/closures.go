079dde0b7c4672f97d18930fb47d6000e1c7d755
GleanCoder <adityakiranacharya0@gmail.com>
2026-08-23 23:01:23 +0530

    learn closure in golang

--- Concepts/8_closures/closures.go
+++ Concepts/8_closures/closures.go
@@ -1,3 +1,23 @@
-// closure in golang is the function value that references variables from outside its body. The function may access and assign to the referenced variables; in this sense the function is "bound" to the variables.
-//  if inner function is dependent on the outer function's variables, then at runtime the binding  of the outer function's variables to the inner function is maintained, even after the outer function has returned. This is because the inner function maintains a reference to the outer function's variables, allowing it to access and modify them even after the outer function has completed execution.
+package main
 
+import "fmt"
+
+/*
+- closure in golang is the function value that references variables from outside its body. The function may access and assign to the referenced variables; in this sense the function is "bound" to the variables.
+- if inner function is dependent on the outer function's variables, then at runtime the binding  of the outer function's variables to the inner function is maintained, even after the outer function has returned. This is because the inner function maintains a reference to the outer function's variables, allowing it to access and modify them even after the outer function has completed execution.
+
+*/
+
+func main() {
+	usedAmount := allocatedBudget()
+	fmt.Println(usedAmount(200)) //800
+}
+
+func allocatedBudget() func(amount int) int {
+	budgetAmount := 1000
+
+	return func(amount int) int {
+		budgetAmount = budgetAmount - amount
+		return budgetAmount
+	}
+}

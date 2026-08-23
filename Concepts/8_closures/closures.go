232e6d7bcc28f572db9c7e376deff311ec9af528
GleanCoder <adityakiranacharya0@gmail.com>
2026-08-23 21:52:00 +0530

    add closures note

---
+++ Concepts/8_closures/closures.go
@@ -0,0 +1,3 @@
+// closure in golang is the function value that references variables from outside its body. The function may access and assign to the referenced variables; in this sense the function is "bound" to the variables.
+//  if inner function is dependent on the outer function's variables, then at runtime the binding  of the outer function's variables to the inner function is maintained, even after the outer function has returned. This is because the inner function maintains a reference to the outer function's variables, allowing it to access and modify them even after the outer function has completed execution.
+

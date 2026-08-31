28dba55475009509bf40f83faf7b6354dd902b29
GleanCoder <adityakiranacharya0@gmail.com>
2026-08-31 12:00:03 +0530

    learn interface in golang

--- Concepts/12_interface/interface.go
+++ Concepts/12_interface/interface.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
-type razorpay struct{}
+// by doing in this struct way we have call pay method on evey instance and we have to modify the code for every payment gateway.
+// to solve this we can use interface.
 
-func (r razorpay) pay(amount float32) {
-	fmt.Println("Payment of", amount, "is done via Razorpay!")
+type paymentGateway interface {
+	pay(amount float32)
+}
+
+// type payment struct {
+// }
+
+// func (p payment) makePayment(amount float32) {
+// 	razorpayPaymentGw := razaorpay{}
+// 	razorpayPaymentGw.pay(amount)
+// }
+
+func checkOutPayment(gateway paymentGateway, amount float32) {
+	gateway.pay(amount)
+
+}
+
+type razaorpay struct{}
+
+func (r razaorpay) pay(amount float32) {
+	// razorpay api call and logics
+	fmt.Println("Payment of", amount, "done using Razorpay!")
 }
 
 type stripe struct{}
 
 func (s stripe) pay(amount float32) {
-	fmt.Println("Payment of", amount, "is done via Stripe!")
+	fmt.Println("Payment of", amount, "done using Stripe!")
 }
 
 func main() {
@@ -22,10 +43,18 @@ func main() {
 	*/
 
 	//USE CASE: suppose we want to implement multiple payment gateway service, let's start it with struct then will move to interface.
+	// newPayment := payment{}
+	// newPayment.makePayment(199)
 
-	razorpayGw := razorpay{}
-	razorpayGw.pay(100)
+	razaorpayGW := razaorpay{}
+	checkOutPayment(razaorpayGW, 399)
 
 	stripeGw := stripe{}
-	stripeGw.pay(200)
+	checkOutPayment(stripeGw, 149)
+
+	/*
+		here our payment checkout doesn't depends on any gateway and also any gateway which satisfy the method of our payment interface will automatically get access to our pay(), don't need to use implements keyword like other languages, go will do it automatically if methods signature is same.
+		- by this we follow open for extension and close for modification rule.
+	*/
+
 }

f284e759b690b345ba388a025d3d2f7b7d794587
GleanCoder <adityakiranacharya0@gmail.com>
2026-09-20 12:15:36 +0530

    learn example of creating and writing to a file in Go

--- Concepts/21_files/files.go
+++ Concepts/21_files/files.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
 
 	// here we will practice some operation related  to files, for that we have to need OS package
@@ -28,31 +23,62 @@ func main() {
 	// Reading the file data
 	// way 1: Read data using buffer storage
 
-	file, err := os.Open("example.txt")
+	// file, err := os.Open("example.txt")
 
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()                // close the file after reading the data asap
-	bufferStorage := make([]byte, 12) // buffer is the temporary space created inside the memory to store the data
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer file.Close()                // close the file after reading the data asap
+	// bufferStorage := make([]byte, 12) // buffer is the temporary space created inside the memory to store the data
 
-	d, err := file.Read(bufferStorage) // read method is used to read the data from the file and store it into the buffer
+	// d, err := file.Read(bufferStorage) // read method is used to read the data from the file and store it into the buffer
 
-	if err != nil {
-		panic(err)
-	}
-	for i := 0; i < len(bufferStorage); i++ {
-		fmt.Println("Buffer Data:", d, "=", string(bufferStorage[i]))
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// for i := 0; i < len(bufferStorage); i++ {
+	// 	fmt.Println("Buffer Data:", d, "=", string(bufferStorage[i]))
+	// }
 
 	// way 2: Read data using ReadFile method
 
-	data, err := os.ReadFile("example.txt") // this method is used to read the data from the file and store it into the variable
-	// ReadFile method is not suitable for large files because it reads the entire file into memory, which can lead to high memory usage and potential performance issues. For large files, it's better to read the file in chunks or use a buffered reader.
-	if err != nil {
-		panic(err)
-	}
+	// data, err := os.ReadFile("example.txt") // this method is used to read the data from the file and store it into the variable
+	// // ReadFile method is not suitable for large files because it reads the entire file into memory, which can lead to high memory usage and potential performance issues. For large files, it's better to read the file in chunks or use a buffered reader.
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Println(string(data))
+	// fmt.Println(string(data))
+
+	// we can do file streaming how we used to do in nodejs,
+
+	// lets play with folders
+
+	// folder, err := os.Open("../") // open helps to load an existing folder into memory. here we are opening the current folder using "." dot notation
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer folder.Close() // close the folder after reading the data asap
+
+	// folderInfo, err := folder.ReadDir(-1) // this returns use the information about the folder
+
+	// for _, file := range folderInfo {
+
+	// 	fmt.Println("files:", file) // return folder information
+	// }
+
+	// lets see how we can create a new file and write data into it
+
+	// file, err := os.Create("example_two.txt")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer file.Close()
+
+	// file.WriteString("Hey Go")
+	// file.WriteString(", you're a great language")
 
 }
